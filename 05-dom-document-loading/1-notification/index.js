c8c1afc00bdf2772c46897b46cd264f6e15bf153
Aleksandr Karadzhikov <aleksandr.karadzhikov@homebuddy.com>
2025-07-21 13:34:19 +0500

    [05-dom-document-loading]: impementation of NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,65 @@
-export default class NotificationMessage {
+let isInDOM = false;
 
+export default class NotificationMessage {
+  constructor(message = "", { duration = 1000, type = "success" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+     <div class="notification ${
+       this.type
+     }" style="--value:${this.msToSecondsString(this.duration)}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+                ${this.message}
+            </div>
+        </div>
+    </div>
+    `;
+  }
+
+  startRemoveTimeout() {
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  msToSecondsString(ms) {
+    return `${(ms / 1000).toFixed(1)}s`;
+  }
+
+  render() {
+    const element = document.createElement("div");
+
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (isInDOM) {
+      return;
+    }
+
+    target.appendChild(this.element);
+    isInDOM = true;
+    this.startRemoveTimeout();
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+      isInDOM = false;
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
