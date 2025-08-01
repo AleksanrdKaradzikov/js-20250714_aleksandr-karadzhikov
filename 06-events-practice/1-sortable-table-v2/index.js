ab0cfc429db4ce7a8f138412ee0760ad4f4f1a20
Aleksandr Karadzhikov <aleksandr.karadzhikov@homebuddy.com>
2025-08-01 19:22:38 +0500

    [06-events-practice]: implement SortableTable v2 component

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,53 @@
-export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+import SortableTableBase from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
 
+export default class SortableTable extends SortableTableBase {
+  static ascOrder = "asc";
+  static descOrder = "desc";
+
+  constructor(headersConfig, { data = [], sorted = {} } = {}) {
+    super(headersConfig, data, sorted);
+
+    this.createListeners();
+  }
+
+  handleHeaderCellClick = (event) => {
+    const cellElement = event.target.closest(".sortable-table__cell");
+
+    if (!cellElement || !cellElement?.hasAttribute("data-sortable")) {
+      return;
+    }
+
+    const sortId = cellElement.dataset.id;
+    const sortOrder = this.getSortOder(cellElement.dataset.order);
+    this.sort(sortId, sortOrder);
+  };
+
+  getSortOder(order) {
+    if (!order) {
+      return SortableTable.ascOrder;
+    }
+
+    return order === SortableTable.ascOrder
+      ? SortableTable.descOrder
+      : SortableTable.ascOrder;
+  }
+
+  createListeners() {
+    this.subElements.header.addEventListener(
+      "click",
+      this.handleHeaderCellClick
+    );
+  }
+
+  destroyListeners() {
+    this.subElements.header.removeEventListener(
+      "click",
+      this.handleHeaderCellClick
+    );
+  }
+
+  destroy() {
+    super.destroy();
+    this.destroyListeners();
   }
 }
